a5f06247534011efb9d843cd032084960529f67d
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2017-11-22 14:32:16 -0200

    Remove tags redundantes e adiciona alt na imagem
    Se as tags 'og:title', 'og:description' e 'og:image' estiverem
    disponíveis, o twitter não necessita das tags 'twitter:*' correspondentes

--- spec/seo_spec.rb
+++ spec/seo_spec.rb
@@ -9,42 +9,42 @@ describe 'Header SEO tags' do
 
     {
       'name="twitter:site"': config['twitter_username'],
-      'name="twitter:title"': config['title'],
-      'name="twitter:description"': config['description'],
-      'name="twitter:image"': config['share_image'],
+      'name="twitter:creator"': config['twitter_username'],
       'name="twitter:card"': "summary_large_image",
       'property="og:type"': "website",
       'property="og:site_name"': config['title'],
       'property="og:url"': config['url'],
       'property="og:title"': config['title'],
       'property="og:description"': config['description'],
-      'property="og:image"': config['share_image']
+      'property="og:image"': config['share_image'],
+      'property="og:image:alt"': config['share_image_alt']
     }.each do |name, content|
       it "contains <meta #{name}>" do
         meta_tags = doc.xpath(%Q{/html/head/meta[@#{name}]})
-        
+
         expect(meta_tags.size).to eq(1)
         expect(meta_tags[0]['content']).to include(content)
       end
     end
 
-    it 'contains <meta name="twitter:site"> with username starting with @ symbol' do
-      meta_tags = doc.xpath('/html/head/meta[@name="twitter:site"]')
-      
-      expect(meta_tags[0]['content']).to match(/^@[^@]/)
+    ['name="twitter:site"', 'name="twitter:creator"'].each do |name|
+      it "contains <meta #{name}> with username starting with @ symbol" do
+        meta_tags = doc.xpath("/html/head/meta[@#{name}]")
+
+        expect(meta_tags[0]['content']).to match(/^@[^@]/)
+      end
     end
 
     it "has an existing share_image" do
       expect(File.exists?(config['share_image'])).to be_truthy
     end
 
-    ['name="twitter:image"', 'property="og:image"'].each do |attr|
-      it "contains <meta #{attr}> with absolute url" do
-        meta_tags = doc.xpath(%Q{/html/head/meta[@#{attr}]})
-        absolute_url = File.join(config['url'], config['share_image'])
+    it 'contains <meta property="og:image"> with absolute url' do
+      meta_tags = doc.xpath('/html/head/meta[@property="og:image"]')
+      absolute_url = File.join(config['url'], config['share_image'])
 
-        expect(meta_tags[0]['content']).to eq(absolute_url)
-      end
+      expect(meta_tags[0]['content']).to eq(absolute_url)
     end
+
   end
 end
