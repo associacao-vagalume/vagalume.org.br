d8ee7ec926cffb11f88e8201569a347a4a626d7e
Travis CI User <travis@example.org>
2017-12-06 18:28:31 +0000

    Armazena versão v1.06122017 do site
    Esta versão foi gerada a partir do commit 37a3332
    
    commit 37a3332a3620d6e5b69e0bc86284a7d0618fa089
    Author: Pedro Manoel Evangelista <pevange@thoughtworks.com>
    Date:   Wed Dec 6 11:42:34 2017 -0200
    
        Move o conteúdo da home para index.html

---
+++ _site/assets/js/home.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  var tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
+
+$( window ).on('load', function () {
+  $('.play.desktop')
+    .removeClass('invisible')
+    .addClass('d-md-block');
+});
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '694',
+    playerVars: {
+      autoplay: 0,
+      loop: 1,
+      controls: 1,
+      showinfo: 0,
+      autohide: 1,
+      modestbranding: 1,
+      vq: 'hd1080',
+      hd: '1'
+    },
+    videoId: 'xt2qJwwnH2U',
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  $('#player').addClass('embed-responsive-item');
+  $('#modal').on('shown.bs.modal', function () {
+    player.playVideo();
+  });
+  $('#modal').on('hidden.bs.modal', function () {
+    player.stopVideo();
+  });
+}
