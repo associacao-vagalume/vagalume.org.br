a6777c6e4d2e54f591b2302ead25af1ac0acddad
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2018-01-12 17:00:57 -0200

    Substitui --quiet por -q pois opção não reconhecida no alpine linux

--- _script/smoke.sh
+++ _script/smoke.sh
@@ -133,7 +133,7 @@ smoke_assert_code_ok() {
 smoke_assert_body() {
     STRING="$1"
 
-    smoke_response_body | grep --quiet "$STRING"
+    smoke_response_body | grep -q "$STRING"
 
     if [[ $? -eq 0 ]]; then
         _smoke_success "Body contains \"$STRING\""
@@ -145,7 +145,7 @@ smoke_assert_body() {
 smoke_assert_headers() {
     STRING="$1"
 
-    smoke_response_headers | grep --quiet "$STRING"
+    smoke_response_headers | grep -q "$STRING"
 
     if [[ $? -eq 0 ]]; then
         _smoke_success "Headers contain \"$STRING\""
