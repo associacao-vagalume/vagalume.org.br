4354eee5516b1b329e70716ebace8cbb0a6e0f14
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2019-02-01 09:20:02 -0200

    Volta para o GTM

--- _spec/index_spec.rb
+++ _spec/index_spec.rb
@@ -3,10 +3,16 @@ require 'rspec'
 describe 'Google Tag Manager' do
   context 'quando o ID do GTM está definido no arquivo _data/google.yml' do
     let(:doc) { load_home }
-    let(:google_id) { load_data('google').fetch('aw') }
+    let(:google_id) { load_data('google').fetch('gtm') }
 
     it 'define a tag <script> dentro de <head>' do
       elements = doc.xpath(%Q{/html/head/script[contains(., "#{google_id}")]})
+
+      expect(elements.size).to eq(1)
+    end
+
+    it 'define a tag <noscript> dentro do <body>' do
+      elements = doc.xpath(%Q{/html/body/*[1]/iframe[contains(@src, "#{google_id}")]})
 
       expect(elements.size).to eq(1)
     end
